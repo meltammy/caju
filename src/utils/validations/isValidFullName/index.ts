08bf79014c6a260b82468b29333dc89ede55f17a
meltammy <mel.tammy2@gmail.com>
2024-09-23 22:42:01 -0300

    feat: add fullName validation

---
+++ src/utils/validations/isValidFullName/index.ts
@@ -0,0 +1,44 @@
+/**
+ * Checks if the string contains at least one space.
+ * @param {string} str - The string to be checked.
+ * @returns {boolean} - Returns true if there is at least one space, otherwise false.
+ */
+function hasAtLeastOneSpace(str: string): boolean {
+  const regex = /\s/;
+  return regex.test(str);
+}
+
+/**
+ * Checks if all words in a string have at least 2 characters.
+ * @param {string} input - The string to be checked.
+ * @returns {boolean} - Returns true if all words have at least 2 characters, otherwise false.
+ */
+function areAllWordsAtLeastTwoCharacters(input: string): boolean {
+  const regex = /^\s*(\w{2,}\s+)*\w{2,}\s*$/;
+  return regex.test(input);
+}
+
+/**
+ * Checks if no word in a string starts with a number.
+ * @param {string} input - The string to be checked.
+ * @returns {boolean} - Returns true if no word starts with a number, otherwise false.
+ */
+function areAllWordsNotStartingWithNumber(input: string): boolean {
+  const regex = /^\s*(?!\d)(\w+\s+)*(?!\d)\w+\s*$/;
+  return regex.test(input);
+}
+
+/**
+ * Validates if a full name meets various conditions.
+ * @param {string} fullName - The full name to be validated.
+ * @returns {boolean} - Returns true if the full name is valid, otherwise false.
+ */
+export function isValidFullName(fullName: string): boolean {
+  const validations = [
+    hasAtLeastOneSpace,
+    areAllWordsAtLeastTwoCharacters,
+    areAllWordsNotStartingWithNumber,
+  ];
+
+  return validations.every((validation) => validation(fullName));
+}
