c56fe01632ec13518093f74ed2b2badb858c1fc3
meltammy <mel.tammy2@gmail.com>
2024-09-24 00:49:34 -0300

    feat(textfield): implement FieldWrapper

--- src/components/Forms/TextField/index.tsx
+++ src/components/Forms/TextField/index.tsx
@@ -1,69 +1,37 @@
 import { InputHTMLAttributes } from "react";
-import { FieldValues, Path, UseFormRegister } from "react-hook-form";
-import styled from "styled-components";
-
-export const Input = styled.input`
-  padding: 0 8px;
-  vertical-align: middle;
-  border-radius: 2px;
-  width: 100%;
-  min-height: 36px;
-  background-color: ${({ theme }) => theme.colors.white};
-  border: 1px solid rgba(36, 28, 21, 0.3);
-  transition: all 0.2s ease-in-out 0s;
-  font-size: 16px;
-  line-height: 18px;
-  font-weight: normal;
-  border-radius: 8px;
-  box-sizing: border-box;
-
-  &:focus {
-    outline: none;
-    border: 1px solid ${({ theme }) => theme.colors.petroleumBlue};
-    box-shadow: inset 0 0 0 1px ${({ theme }) => theme.colors.petroleumBlue};
-  }
-`;
-
-type UseFormProps<FormData extends FieldValues> =
-  | {
-      name: Path<FormData>;
-      register: UseFormRegister<FormData>;
-    }
-  | {
-      name: never;
-      register: never;
-    };
+import { FieldValues } from "react-hook-form";
+import { RegisterFormProps } from "../types";
+import { Input } from "./styles";
+import { FieldWrapper } from "../FieldWrapper";
 
 export type TextFieldProps<FormData extends FieldValues> = {
   label?: string;
   error?: string;
   id: string;
 } & InputHTMLAttributes<any> &
-  UseFormProps<FormData>;
+  RegisterFormProps<FormData>;
 
 function TextField<FormData extends FieldValues>({
   register,
   name,
   error,
+  id,
+  label,
   ...props
 }: TextFieldProps<FormData>) {
   const registerProps = register ? register(name) : undefined;
 
-  const helpId = `${props.id}-help`;
+  const helpId = `${id}-help`;
 
   return (
-    <div>
-      <label htmlFor={props.id}>{props.label}</label>
+    <FieldWrapper id={id} label={label} error={error} helpId={helpId}>
       <Input
         {...props}
         {...registerProps}
         aria-invalid={!!error}
         aria-describedby={helpId}
       />
-      <span role="alert" id={helpId} style={{ fontSize: 12, color: "red" }}>
-        {error}
-      </span>
-    </div>
+    </FieldWrapper>
   );
 }
 
