83bac3906d29258bf722cc2712e8b039110c4d42
meltammy <mel.tammy2@gmail.com>
2024-09-24 13:49:17 -0300

    test(asyncbutton): add test id

--- src/components/Buttons/AsyncButton/index.tsx
+++ src/components/Buttons/AsyncButton/index.tsx
@@ -6,9 +6,14 @@ type Props = {
   isLoading?: boolean;
 } & ComponentProps<typeof Button>;
 
+export const asyncButtonTestId = "async-button";
 export function AsyncButton({ isLoading, children, disabled, ...rest }: Props) {
   return (
-    <Button {...rest} disabled={isLoading || disabled}>
+    <Button
+      {...rest}
+      disabled={isLoading || disabled}
+      data-testid={asyncButtonTestId}
+    >
       {isLoading ? <StyledSpinner /> : children}
     </Button>
   );
