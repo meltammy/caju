adc71091ed8483d81ceeda6fbd1d3ced28996375
meltammy <mel.tammy2@gmail.com>
2024-09-25 19:11:36 -0300

    style: add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import Router from "~/router";
 import { Header } from "./components/Header";
 import { ThemeProvider } from "styled-components";
 import { theme } from "../styles/theme";
+import { GlobalStyle } from "../styles/globals";
 import { ToastProvider } from "./components/Toast/ToastProvider";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Header>
-        <h1>Caju Front Teste [deploy test 2]</h1>
-      </Header>
+      <GlobalStyle />
+      <Header />
       <ToastProvider>
         <Router />
       </ToastProvider>
---
+++ styles/globals.ts
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+html, body{
+  padding: 0;
+  margin: 0;
+  font-size: 1rem;
+  font-family: "Poppins", sans-serif;
+}
+`;
