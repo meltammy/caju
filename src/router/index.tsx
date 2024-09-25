4e0fe7c6a1f299bfadffb3fc532882fc544e3917
meltammy <mel.tammy2@gmail.com>
2024-09-25 00:52:41 -0300

    fix(router): fix the redirection behavior

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,4 +1,4 @@
-import { HashRouter, Redirect, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 import routes from "./routes";
 import DashboardPage from "~/pages/Dashboard";
 import NewRegistration from "~/pages/NewRegistration";
@@ -6,21 +6,15 @@ import NewRegistration from "~/pages/NewRegistration";
 const Router = () => {
   return (
     <div style={{ marginTop: 64 }}>
-      <HashRouter basename="/caju/">
+      <BrowserRouter basename="/caju/">
         <Switch>
           <Route exact path={routes.dashboard} component={DashboardPage} />
           <Route exact path={routes.newUser} component={NewRegistration} />
-          <Route
-            exact
-            path={routes.history}
-            component={() => <div>History</div>}
-          />
-
           <Route exact path="*">
             <Redirect to={routes.dashboard} />
           </Route>
         </Switch>
-      </HashRouter>
+      </BrowserRouter>
     </div>
   );
 };
