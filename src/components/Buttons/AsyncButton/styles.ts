688ffd894fa3e1f4dec35aea7b460c67deef9690
meltammy <mel.tammy2@gmail.com>
2024-09-26 20:46:45 -0300

    style(AsyncButton): fix spinner size

--- src/components/Buttons/AsyncButton/styles.ts
+++ src/components/Buttons/AsyncButton/styles.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { Spinner } from "~/components/Spinner";
 
 export const StyledSpinner = styled(Spinner)`
-  width: 0.3rem;
-  height: 0.3rem;
+  width: 1.3rem;
+  height: 1.3rem;
   border-width: 0.25rem;
 `;
