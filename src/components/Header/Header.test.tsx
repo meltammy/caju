6c39dbd400738937ee248a503d84f2bb6f8345cf
meltammy <mel.tammy2@gmail.com>
2024-09-25 21:04:34 -0300

    test: update snapshots

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,25 +1,20 @@
 import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import { Header } from ".";
 import { ThemeProvider } from "styled-components";
 import { theme } from "~/../styles/theme";
 
-const renderHeader = (children: React.ReactNode) => {
+const renderHeader = () => {
   return render(
     <ThemeProvider theme={theme}>
-      <Header>{children}</Header>
+      <Header />
     </ThemeProvider>
   );
 };
 
 describe("Header component", () => {
-  it("should contains the title", () => {
-    renderHeader(<h1>Test Header</h1>);
-    expect(screen.getByText("Test Header")).toBeInTheDocument();
-  });
-
   it("should match snapshot", () => {
-    const { asFragment } = renderHeader(<h1>Test Header</h1>);
+    const { asFragment } = renderHeader();
     expect(asFragment()).toMatchSnapshot();
   });
 });
